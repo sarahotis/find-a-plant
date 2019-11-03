5b46e411a8d0017d47dd047f9579772c1b5b1494
apenghat <ananth.penghat@gmail.com>
2019-11-03 17:58:40 -0500

    Add location checking/handling to report plant page

--- app/src/main/java/com/example/findaplant/ReportPlantActivity.kt
+++ app/src/main/java/com/example/findaplant/ReportPlantActivity.kt
@@ -1,5 +1,52 @@
 package com.example.findaplant
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
-class ReportPlantActivity : AppCompatActivity()
+class ReportPlantActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d("ananth", "before permission stufff")
+        requestPermissions()
+        Log.d("ananth", "after permission stufff")
+    }
+
+    private fun requestPermissions() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+            && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,  arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), MY_PERMISSIONS_REQUEST_LOCATION)
+        } else {
+            setContentView(R.layout.report_plant_layout)
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            MY_PERMISSIONS_REQUEST_LOCATION -> {
+                // If request is cancelled, the result arrays are empty.
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                    setContentView(R.layout.report_plant_layout)
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    setContentView(R.layout.report_plant_no_location)
+                }
+                return
+            }
+        }
+    }
+
+    companion object {
+        val MY_PERMISSIONS_REQUEST_LOCATION = 1
+    }
+
+}
