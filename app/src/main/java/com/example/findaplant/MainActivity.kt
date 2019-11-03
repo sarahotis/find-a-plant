eece8ea06f95b841e41f7b5508b9459c994b7eb8
Sarah <sarah@wireless-10-105-87-220.umd.edu>
2019-11-03 16:12:23 -0500

    colors and main activity

--- app/src/main/java/com/example/findaplant/MainActivity.kt
+++ app/src/main/java/com/example/findaplant/MainActivity.kt
@@ -1,12 +1,36 @@
 package com.example.findaplant
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
+
+    internal var reportBtn: Button? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initializeViews()
+
+        reportBtn!!.setOnClickListener {
+            if (notLoggedIn()) {
+                startActivity(Intent(this@MainActivity, LoginActivity::class.java))
+            } else {
+                startActivity(Intent(this@MainActivity, ReportPlantActivity::class.java))
+            }
+        }
+
+    }
+
+    private fun initializeViews() {
+        reportBtn = findViewById(R.id.report)
+    }
+
+    private fun notLoggedIn(): Boolean {
+        return true
+        //SharedPreferences
     }
 }
