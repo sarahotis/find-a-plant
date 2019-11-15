e2acc3ccc1fa0bc23d4bc4d760088fa3d7d530d4
root <root@localhost.localdomain>
2019-11-14 22:05:56 -0500

    Added features to SearchActivity

--- app/src/main/java/com/example/findaplant/SearchActivity.kt
+++ app/src/main/java/com/example/findaplant/SearchActivity.kt
@@ -3,14 +3,20 @@ package com.example.findaplant
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.util.Log
+import android.widget.AutoCompleteTextView
+import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import com.google.firebase.database.*
+import java.util.*
+import kotlin.collections.ArrayList
 
 
 class SearchActivity : AppCompatActivity() {
     //search will tap into the Firebase
     internal var searchText: EditText? = null
     private lateinit var databasePlants: DatabaseReference
+    private lateinit var searchPlantButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,33 +29,56 @@ class SearchActivity : AppCompatActivity() {
         databasePlants = database.getReference("Plants Added To FB")
         Log.i("Firebase", "Database plants referenced")
 
-        //Making a plant object to add to Firebase
-        var plant = Plant("Daisy", "White petals, yellow inside", "Ngan")
-        databasePlants.setValue(plant)
+        //get search text
 
 
+
+        //Set on click listener for Search Button
+        searchPlantButton.setOnClickListener {
+            var plantToSearch = searchText!!.text.toString()
+            Log.i("SearchActivity", "Plant search " + plantToSearch)
+
+            if(plantToSearch == ""){
+                val pleaseEnterPlantText = Toast.makeText(applicationContext, "Please provide a plant.", Toast.LENGTH_SHORT)
+                pleaseEnterPlantText.show()
+            }else{
+                //call searchPlant method
+                searchPlant(plantToSearch)
+            }
+        }
+
+
+//        //Making a plant object to add to Firebase
+//        var plant = Plant("Daisy", "White petals, yellow inside", "Ngan")
+//        databasePlants.setValue(plant)
 
     }
 
-    override fun onStart() {
-        super.onStart()
+    //Search firebase for plant if found then start DescriptionActivity. Else make toast that plant
+    //isn't found
+    private fun searchPlant(plantText : String){
+        //Making a plant object to add to Firebase
+        var plant = Plant("Cosmos", "Purple petals, yellow inside", "Ngan")
+        databasePlants.setValue(plant)
         Log.i("Firebase", "onStart started in Search Activity")
         databasePlants.addValueEventListener(object : ValueEventListener{
             override fun onDataChange(dataSnapshot: DataSnapshot) {
-                val value = dataSnapshot.getValue(Plant::class.java)
-                Log.i("Firebase", "Name is ${value!!.plantName}")
-                Log.i("Firebase", "Name is ${value!!.plantDescription}")
-                Log.i("Firebase", "Name is ${value!!.user}")
+               for(postSnapshot in dataSnapshot.children){
+                   var plant = postSnapshot.getValue(String::class.java)
+                   Log.i("SearchActivity", "Plant name is " + plant)
+               }
             }
 
             override fun onCancelled(error: DatabaseError) {
                 Log.w("Firebase", "Failed to read data", error.toException())
             }
         })
+        //search Firebase
 
     }
 
     private fun initializeViews() {
         searchText = findViewById(R.id.plant_search_text)
+        searchPlantButton = findViewById(R.id.plant_search_button)
     }
 }
